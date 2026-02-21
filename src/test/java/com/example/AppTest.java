ea9289b960a45c37758d9308efdc8fdd98a19ecd
Barun-Mahato <barunkumar@gmail.com>
2026-02-21 18:14:35 +0530

    Initial Maven project with TestNG test

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+    @Test
+    public void sampleTest() {
+        System.out.println("Test ran successfully!");
+    }
+}
